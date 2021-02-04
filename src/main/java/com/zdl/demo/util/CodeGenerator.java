0fd34b08ba5defe09e8f495e14569b8476e5a262
deli.zhou@qq.com <deli.zhou@qq.com>
2021-02-04 17:59:45 +0800

    Mybatis-plus 代码自动生成

--- src/main/java/com/zdl/demo/util/CodeGenerator.java
+++ src/main/java/com/zdl/demo/util/CodeGenerator.java
@@ -1,9 +1,12 @@
 package com.zdl.demo.util;
 
 import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.ConstVal;
 import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
@@ -15,8 +18,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Component
@@ -51,6 +56,23 @@ public class CodeGenerator {
         if (CollUtil.isNotEmpty(tableNames)) {
             customStrategyConfig.setInclude(tableNames.toArray(new String[0]));
         }
+//        "entity_path" -> "E:\workspace\demo\code-generator-demo\src\main\java\com\zdl\demo2\entity"
+//        "controller_path" -> "E:\workspace\demo\code-generator-demo\src\main\java\com\zdl\demo2\controller"
+//        "xml_path" -> "E:\workspace\demo\code-generator-demo\src\main\java\com\zdl\demo2\mapper\xml"
+//        "mapper_path" -> "E:\workspace\demo\code-generator-demo\src\main\java\com\zdl\demo2\mapper"
+//        "service_impl_path" -> "E:\workspace\demo\code-generator-demo\src\main\java\com\zdl\demo2\service\impl"
+//        "service_path" -> "E:\workspace\demo\code-generator-demo\src\main\java\com\zdl\demo2\service"
+        Map<String, String>  pathInfo = CollectionUtils.newHashMapWithExpectedSize(6);
+        String userDir = System.getProperty("user.dir");
+        String configParent = customPackageConfig.getParent();
+        String configEntity = customPackageConfig.getEntity();
+        pathInfo.put(ConstVal.ENTITY_PATH, joinPath(userDir + "\\src\\main\\java\\", configParent, Strings.EMPTY, configEntity));
+        pathInfo.put(ConstVal.MAPPER_PATH, joinPath(userDir + "\\src\\main\\java\\", configParent, Strings.EMPTY, customPackageConfig.getMapper()));
+        pathInfo.put(ConstVal.SERVICE_PATH, joinPath(userDir + "\\src\\main\\java\\", configParent, Strings.EMPTY, customPackageConfig.getService()));
+        pathInfo.put(ConstVal.SERVICE_IMPL_PATH, joinPath(userDir + "\\src\\main\\java\\", configParent, Strings.EMPTY, customPackageConfig.getServiceImpl()));
+        pathInfo.put(ConstVal.CONTROLLER_PATH, joinPath(userDir + "\\src\\main\\java\\", configParent, Strings.EMPTY, customPackageConfig.getController()));
+        pathInfo.put(ConstVal.XML_PATH, joinPath(userDir + "\\src\\main\\resources\\mapper\\", Strings.EMPTY, customPackageConfig.getModuleName(), Strings.EMPTY));
+        customPackageConfig.setPathInfo(pathInfo);
 
         generator.setGlobalConfig(customGlobalConfig);
         generator.setDataSource(customDataSourceConfig);
@@ -58,28 +80,20 @@ public class CodeGenerator {
         generator.setTemplate(customTemplateConfig);
         generator.setStrategy(customStrategyConfig);
 
-        InjectionConfig injectionConfig = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                // to do nothing
-            }
-        };
-        // 自定义输出配置
-        List<FileOutConfig> fileOutConfigList = new ArrayList<>();
-        // 自定义配置会被优先输出
-        fileOutConfigList.add(new FileOutConfig() {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                String outputFilePath = System.getProperty("user.dir") + "\\src\\main\\resources\\mapper\\";
-                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return outputFilePath + customPackageConfig.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-        injectionConfig.setFileOutConfigList(fileOutConfigList);
-        generator.setCfg(injectionConfig);
-
         generator.setTemplateEngine(new FreemarkerTemplateEngine());
         generator.execute();
+    }
+
+    private String joinPath(String parentDir, String parent, String moduleName, String packageName) {
+        if (StringUtils.isBlank(parentDir)) {
+            parentDir = System.getProperty("user.dir");
+        }
+        if (!StringUtils.endsWith(parentDir, File.separator)) {
+            parentDir += File.separator;
+        }
+        packageName = packageName.replaceAll("\\.", StringPool.BACK_SLASH + File.separator);
+        parent = parent.replaceAll("\\.", StringPool.BACK_SLASH + File.separator);
+        moduleName = moduleName.replaceAll("\\.", StringPool.BACK_SLASH + File.separator);
+        return parentDir + parent + moduleName + File.separator + packageName;
     }
 }
